f784a8aca6b80d5b30c696c73885a79fb4ac4a66
SaadUsmani <126378656+Muhammad-Saad-Usmani7@users.noreply.github.com>
2023-10-25 00:10:00 +0500

    Add files via upload
    continue

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const apikey = "1df1779b66d82802e067653a7057ac0c";
   const button = document.getElementById("button");
   const temp = document.getElementById("temp");
   const humidity = document.getElementById("humid");
+  const winds = document.getElementById("wind");
+  const pic = document.getElementById("picture");
 
   button.addEventListener("click", async () => {
     let cityname = search1.value;
@@ -19,7 +21,32 @@ const apikey = "1df1779b66d82802e067653a7057ac0c";
       console.log(result);
 
 	temp.innerText = (result.main.temp-273.16).toFixed(2);
+  winds.innerText = (result.wind.speed+" Km/h");
   humidity.innerText = result.main.humidity+"%";
+
+  if(result.weather[0].description == 'smoke')
+  {
+    pic.src = 'smoke-icon.png';
+  }
+  else if(result.weather[0].description == 'scattered clouds')
+  {
+    pic.src = 'scattered-clouds.png';
+  }
+  else if(result.weather[0].description == 'clear sky')
+  {
+    pic.src = 'clear-sky.jpg';
+  }
+  else if(result.weather[0].description == 'few clouds')
+  {
+    pic.src = 'few clouds.jpg';
+  }
+  else if(result.weather[0].description == 'fog')
+  {
+    pic.src = 'fog.jpg';
+  }
+
+
+
     } catch (error) {
       alert(error);
     }
